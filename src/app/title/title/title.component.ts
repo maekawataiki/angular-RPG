47d21ca51885737600fe3187946afec5931a2486
maekawataiki <maekawataiki@yahoo.co.jp>
2018-11-26 15:57:11 +0800

    minor change

--- src/app/title/title/title.component.ts
+++ src/app/title/title/title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -6,22 +6,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title.component.scss']
 })
 export class TitleComponent implements OnInit {
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
   showmenu = false;
   option = 0;
+
+  text = "";
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggleMenu(){
+  toggleMenu() {
     this.showmenu = !this.showmenu;
     this.option = 0;
   }
 
-  toggleOption(param: number){
+  toggleOption(param: number) {
     if (this.option != param) this.option = param;
     else this.option = 0;
+  }
+
+  action() {
+    this.option = 0;
+    this.showmenu = false;
+    this.text += "\n";
+    this.scrollToBottom();
+  }
+
+  attack(param: string) {
+    this.text += "Attacked by " + param + ": " + Math.floor(Math.random() * 100) + " damage to UNKNOWN";
+    this.action();
+  }
+
+  magic(param: string) {
+    this.text += "Used " + param + ": " + Math.floor(Math.random() * 100) + " damage to UNKNOWN";
+    this.action();
+  }
+
+  item(param: string) {
+    this.text += "Used " + param + ": ";
+    if (param == 'Portion') this.text += "gained " + Math.floor(Math.random() * 100) + " HP";
+    else if (param == 'Monster Ball') this.text += "nothing happened";
+    this.action();
+  }
+
+  guard(param: string) {
+    this.text += "Guard";
+    this.action();
+  }
+
+  sleep(param: string) {
+    this.text += "Slept: gained " + Math.floor(Math.random() * 100) + " HP";
+    this.action();
+  }
+
+  escape(param: string) {
+    this.text += "Cannot escape!";
+    this.action();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) { }
   }
 
 }
