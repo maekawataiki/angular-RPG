c5d110c429da634d471aa1f223623948785d7135
maekawataiki <maekawataiki@yahoo.co.jp>
2018-11-26 16:04:38 +0800

    minor change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'title', pathMatch: 'full' },
-  { path: 'title', loadChildren: "./title/title.module#TitleModule" },
-  { path: '**', redirectTo: 'title' }
+  { path: '', loadChildren: "./title/title.module#TitleModule", pathMatch: 'full' },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
